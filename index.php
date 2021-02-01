932c8c33f4ef95396de6f70d0fa65fa4d6c2cd1a
YensyGomez <yensy@sirius.utp.edu.co>
2021-02-01 18:18:35 +0100

    grid con elementos centrados horizpntal y verticalmente

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/index.css" type="text/css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<div class='container'>
+  <div class="box"><h1>IT</h1></div>
+  <div class="box"><h1>A</h1></div>
+  <div class="box"><h1>CA</h1></div>
+  <div class="box"><h1>DE</h1></div>
+  <div class="box"><h1>MY </h1></div>
+</div>
+    
+</body>
+</html>
